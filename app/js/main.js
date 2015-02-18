19804ebf6d68d26830ddd8c953d9de0b2118ba31
YOUR NAME <erinpagemd@yahoo.com>
2015-02-17 22:33:56 -0600

    most html written

---
+++ app/js/main.js
@@ -0,0 +1,48 @@
+/* jshint node: true */
+
+//var $ = require('jquery'),
+//    _ = require('lodash'),
+//    Firebase = require('firebase');
+
+// var FIREBASE_URL = 'https://nerd-tree.firebaseio.com',
+//     fb = new Firebase(FIREBASE_URL),
+//     usersFbUrl;
+//
+// $(document).ready(initialize);
+// function initialize () {
+//
+  ////////////////////////////////////////////////////
+  ///////////////// Initial State ////////////////////
+  ///////////////////////////////////////////////////
+
+  //hide signup form
+  //$('.sign-up-form').hide();
+
+
+  ////////////////////////////////////////////////////
+  ///////////////// Click Events /////////////////////
+  ///////////////////////////////////////////////////
+
+  //signIn button on navbar
+  // $('#signIn').click();
+  // //signUp button on navbar
+  // $('#signUp').click(showSignUpForm);
+  //
+  // //signUp form
+  // $('#createNewUser').click();
+  // $('#cancelSignup').click();
+  //
+  //signin form
+// }
+
+////////////////////////////////////////////////////
+///////////////// Functions // /////////////////////
+///////////////////////////////////////////////////
+
+//show sign up form and hide the brand-land
+// function showSignUpForm (event) {
+//   event.preventDefault();
+//
+//   //hide brand-land
+//   $('.band-land').toggle();
+// }
