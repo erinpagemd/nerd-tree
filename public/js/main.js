031b83ead73c76fb65874a09d26c9758bc9a8d2f
YOUR NAME <erinpagemd@yahoo.com>
2015-02-18 00:28:20 -0600

    creates new user

--- public/js/main.js
+++ public/js/main.js
@@ -4,19 +4,23 @@
 //    _ = require('lodash'),
 //    Firebase = require('firebase');
 
-// var FIREBASE_URL = 'https://nerd-tree.firebaseio.com',
-//     fb = new Firebase(FIREBASE_URL),
-//     usersFbUrl;
-//
-// $(document).ready(initialize);
-// function initialize () {
-//
+var FIREBASE_URL = 'https://nerd-tree.firebaseio.com',
+    usersFbUrl;
+
+$(document).ready(initialize);
+function initialize () {
+
   ////////////////////////////////////////////////////
   ///////////////// Initial State ////////////////////
   ///////////////////////////////////////////////////
 
-  //hide signup form
-  //$('.sign-up-form').hide();
+  //when you first come to the page, hide certain things if not logged in
+  //signup form
+  $('.sign-up-form').hide();
+  //signin form
+  $('.sign-in-form').hide();
+  //profile form
+  $('.profile-form').hide();
 
 
   ////////////////////////////////////////////////////
@@ -24,25 +28,62 @@
   ///////////////////////////////////////////////////
 
   //signIn button on navbar
-  // $('#signIn').click();
-  // //signUp button on navbar
-  // $('#signUp').click(showSignUpForm);
-  //
+  $('#signIn').click(showSignInForm);
+  //signUp button on navbar
+  $('#signUp').click(showSignUpForm);
+
   // //signUp form
-  // $('#createNewUser').click();
+  $('#createNewUser').click(createNewUser);
   // $('#cancelSignup').click();
   //
   //signin form
-// }
+}//end of initialize
 
 ////////////////////////////////////////////////////
 ///////////////// Functions // /////////////////////
 ///////////////////////////////////////////////////
 
+//create a new firebase user
+function createNewUser (event) {
+  event.preventDefault();
+  //get the email
+  var $email = $('#signupEmail').val();
+  // and password
+  var $password = $('#signupPassword').val();
+  //create the login object
+  var loginObj = {
+    email: $email,
+    password: $password
+  }
+  //clear the values of the inputs
+  $('#signupEmail').val('');
+  $('#signupPassword').val('');
+
+  var fb = new Firebase(FIREBASE_URL);
+  fb.createUser(loginObj, function (error, userData) {
+    if (error) {
+      console.log("Error creating user: ", error);
+      alert("Rejected! ", error);
+    } else {
+      console.log("Successfully created user account with uid: ", userData.uid);
+    }
+  });
+}
+
+//show sign in form and hide the brand-land
+function showSignInForm (event) {
+  event.preventDefault();
+  //show the signin form
+  $('.sign-in-form').toggle();
+  //hide brand-land
+  $('.brand-land').toggle();
+}
+
 //show sign up form and hide the brand-land
-// function showSignUpForm (event) {
-//   event.preventDefault();
-//
-//   //hide brand-land
-//   $('.band-land').toggle();
-// }
+function showSignUpForm (event) {
+  event.preventDefault();
+  //show the signup form
+  $('.sign-up-form').toggle();
+  //hide brand-land
+  $('.brand-land').toggle();
+}
